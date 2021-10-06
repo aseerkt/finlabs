524654612996f013d9b3a7cb68f3622df0ba99f3
Aseer KT <aseermiqdad@gmail.com>
2021-10-06 13:23:46 +0530

    api: populate project fields

--- src/pages/api/projects/[projectId].ts
+++ src/pages/api/projects/[projectId].ts
@@ -4,7 +4,12 @@ import ProjectModel from '@/models/Project';
 export default apiWrapper(async function (req, res) {
   switch (req.method) {
     case 'GET': {
-      const project = await ProjectModel.findOne({ _id: req.query.projectId });
+      const project = await ProjectModel.findOne({
+        _id: req.query.projectId,
+      })
+        .populate('creator boards')
+        .select('-creator.password');
+
       if (!project) {
         return res.status(404).json({ error: 'Project not found' });
       }
